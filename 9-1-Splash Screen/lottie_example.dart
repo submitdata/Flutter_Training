05a8737b17a9d970ec8890d65683411866aff8f0
submitdata <121533882+submitdata@users.noreply.github.com>
2023-01-09 14:25:04 +0530

    Add files via upload

---
+++ 9-1-Splash Screen/lottie_example.dart
@@ -0,0 +1,30 @@
+//@dart=2.9
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+
+
+class MyApp2 extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ListView(
+          children: [
+            // Load a Lottie file from your assets
+            //Lottie.asset('assets/LottieLogo1.json'),
+
+            // Load a Lottie file from a remote url
+            Lottie.network(
+                'https://raw.githubusercontent.com/xvrh/lottie-flutter/master/example/assets/Mobilo/A.json'),
+
+            // Load an animation and its images from a zip file
+           // Lottie.asset('assets/lottiefiles/angel.zip'),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ 9-1-Splash Screen/main.dart
@@ -0,0 +1,61 @@
+//@dart=2.9
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+void main()
+{
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+      return MaterialApp(
+          title:'Splash Screen',
+          theme: ThemeData(
+              primarySwatch: Colors.green,
+          ),
+        home:Splash(),
+        debugShowCheckedModeBanner: false,
+      );
+  }
+
+}
+
+class Splash extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return SplashScreen
+      (
+          seconds:6,
+          navigateAfterSeconds: new SecondScreen(),
+          title:Text('MySplashApp',textScaleFactor: 2,),
+          image:Image.network('https://www.geeksforgeeks.org/wp-content/uploads/gfg_200X200.png'),
+          loadingText: Text("Loading"),
+          photoSize: 100.0,
+          loaderColor: Colors.blue,
+
+      );
+  }
+
+}
+class SecondScreen extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+      return Scaffold(
+
+          appBar: AppBar(
+              title:Text('My Data')
+          ),
+          body:Center(
+              child: Text("Welcome"),
+          )
+      );
+  }
+
+}
